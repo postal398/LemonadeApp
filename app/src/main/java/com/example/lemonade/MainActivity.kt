b852306a390dc40013ef3c413453bad90e7d1062
Vadim <maslovvadim398@gmail.com>
2023-06-08 10:28:51 +0300

    optimeze versions in Gradle

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,30 +1,14 @@
 package com.example.lemonade
 
-import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.RoundRect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.colorspace.Illuminant.A
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.lemonade.data.Data
 import com.example.lemonade.ui.theme.LemonadeTheme
 
@@ -38,6 +22,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
 }
 
 
@@ -50,6 +35,9 @@ fun LemonadeApp() {
 
     val updateStep: (Int) -> Unit = { newStep -> Step = newStep }
     val updateCount: (Int) -> Unit = { newCount -> count = newCount }
+
+
+
 
     when (Step) {
 
